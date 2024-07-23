765e581e4240487e6e5fdb1af775be4d13f31981
Ratandeep Singh <chaddharatandeepsingh2345@gmail.com>
2024-07-23 18:19:50 +0530

    half part almost done

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { User } from "@prisma/client";
+import Image from "next/image";
+import React from "react";
+
+interface AvatarProps {
+  user?: User;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ user }) => {
+  return (
+    <div className="relative">
+      <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-ll">
+        <Image alt="avatar" src={user?.image || "/images/avatar1.jpg"} fill style={{ objectFit: 'cover' }}  />
+      </div>
+      <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
+    </div>
+  );
+};
+
+export default Avatar;
